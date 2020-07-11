a50cde668ae826a01522f01a22faec7aa7940693
Baskaran Ashwin <ashwin.baskaran@wallstreetenglish.com>
2020-07-11 23:08:24 +0530

    handled config

--- socket.js
+++ socket.js
@@ -1,10 +1,17 @@
 import io from 'socket.io-client';
 
+let config = {
+  host : "127.0.0.1",
+  port: 5001
+}
+if(process.env.ENV !== "dev") {
+  config.port = process.env.PORT;
+}
 let socket;
 let status = "DISCONNECTED";
 
 const init = () => {
-  socket = io('http://localhost:5001');
+  socket = io(`http://${config.host}:${config.port}`);
   socket.on('connect', function(){ 
     status = "CONNECTED";
   });
